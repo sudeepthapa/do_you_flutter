3e31631213d1d1f8db40257f61275815a4fa1a12
sudeptech <sudeptech@gmail.com>
2020-02-22 12:39:06 +0545

    form handling

--- lib/ui/pages/add.dart
+++ lib/ui/pages/add.dart
@@ -2,6 +2,9 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:iremember/core/models/image.dart';
+import 'package:provider/provider.dart';
+import 'package:iremember/core/notifiers/image_notifiers.dart';
 
 //TODO allow user to pick image and display the preview in UI
 //TODO save new data to firestore (upload image to storage)
@@ -12,18 +15,19 @@ class AddPage extends StatefulWidget {
 
 class _AddPageState extends State<AddPage> {
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
-
+  bool loading = false;
   String title;
   String description;
   File _image;
 
   @override
   Widget build(BuildContext context) {
+    var imageProvider = Provider.of<ImageNotifiers>(context);
+
     return Scaffold(
       key: scaffoldKey,
       appBar: AppBar(
         title: Text("Add item"),
-        backgroundColor: Colors.blueAccent,
       ),
       body: ListView(
         padding: EdgeInsets.all(10),
@@ -43,7 +47,7 @@ class _AddPageState extends State<AddPage> {
           SizedBox(
             height: 20,
           ),
-          _buildSaveButton(context),
+          _buildSaveButton(context, imageProvider),
           SizedBox(
             height: 20,
           ),
@@ -58,12 +62,32 @@ class _AddPageState extends State<AddPage> {
   }
 
   Widget _imagePreview() {
-    return Container(
-      height: 300,
-      margin: EdgeInsets.only(bottom: 40),
-      width: 300,
-      child: _image != null ? Image.file(_image) : Container(),
-    );
+    return _image != null
+        ? Container(
+            height: 300,
+            margin: EdgeInsets.only(bottom: 40),
+            alignment: Alignment(1, -1),
+            width: 300,
+            decoration: _image != null
+                ? BoxDecoration(
+                    image: DecorationImage(
+                        image: FileImage(_image), fit: BoxFit.contain),
+                  )
+                : BoxDecoration(),
+            child: InkWell(
+              child: Icon(
+                Icons.cancel,
+                color: Colors.red,
+                size: 40,
+              ),
+              onTap: () {
+                setState(() {
+                  _image = null;
+                });
+              },
+            ),
+          )
+        : Container();
   }
 
   TextField _buildTitleField() {
@@ -104,62 +128,84 @@ class _AddPageState extends State<AddPage> {
         label: Text("Add Image"),
         color: Colors.blue,
         onPressed: () {
-          return scaffoldKey.currentState
-              .showBottomSheet((context) => Container(
-                    color: Colors.red,
-                    height: 100,
-                    padding: EdgeInsets.symmetric(horizontal: 100),
-                    child: Row(
-                      children: <Widget>[
-                        IconButton(
-                          icon: Icon(
-                            Icons.camera_alt,
-                            size: 50,
-                            color: Colors.white,
-                          ),
-                          onPressed: () async {
-                            File image = await ImagePicker.pickImage(
-                                source: ImageSource.camera);
-                            setState(() {
-                              _image = image;
-                            });
-                            Navigator.pop(context);
-                          },
-                        ),
-                        Spacer(),
-                        IconButton(
-                          icon: Icon(
-                            Icons.image,
-                            size: 50,
-                            color: Colors.white,
-                          ),
-                          onPressed: () async {
-                            File image = await ImagePicker.pickImage(
-                                source: ImageSource.gallery);
-                            setState(() {
-                              _image = image;
-                            });
-                            Navigator.pop(context);
-                          },
-                        )
-                      ],
+          scaffoldKey.currentState.showBottomSheet((context) => Container(
+                color: Colors.red,
+                height: 200,
+                padding: EdgeInsets.symmetric(horizontal: 100),
+                child: Row(
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(
+                        Icons.camera_alt,
+                        size: 50,
+                        color: Colors.white,
+                      ),
+                      onPressed: () async {
+                        File image = await ImagePicker.pickImage(
+                            source: ImageSource.camera);
+                        setState(() {
+                          _image = image;
+                        });
+                        Navigator.pop(context);
+                      },
                     ),
-                  ));
+                    Spacer(),
+                    IconButton(
+                      icon: Icon(
+                        Icons.image,
+                        size: 50,
+                        color: Colors.white,
+                      ),
+                      onPressed: () async {
+                        File image = await ImagePicker.pickImage(
+                            source: ImageSource.gallery);
+                        setState(() {
+                          _image = image;
+                        });
+                        Navigator.pop(context);
+                      },
+                    )
+                  ],
+                ),
+              ));
         },
       ),
     );
   }
 
-  SizedBox _buildSaveButton(BuildContext context) {
+  SizedBox _buildSaveButton(
+      BuildContext context, ImageNotifiers imageProvider) {
     return SizedBox(
       height: 50,
       width: 20.0,
       child: RaisedButton.icon(
-        icon: Icon(Icons.save),
-        label: Text("Save"),
+        icon: loading ? Container() : Icon(Icons.save),
+        label: loading
+            ? CircularProgressIndicator(
+                backgroundColor: Colors.white,
+              )
+            : Text("Save"),
         color: Colors.blue,
         onPressed: () async {
-          print(_image);
+          if (title == '' || title == null) {
+            return scaffoldKey.currentState.showSnackBar(SnackBar(
+              content: Text("Please add title."),
+            ));
+          }
+          if (description == '' || description == null) {
+            return scaffoldKey.currentState.showSnackBar(SnackBar(
+              content: Text("Please add Description."),
+            ));
+          }
+          setState(() {
+            loading = true;
+          });
+          await imageProvider.addImage(
+              ImageItem(title: title, description: description, url: "test"),
+              _image);
+          setState(() {
+            loading = false;
+          });
           Navigator.pop(context);
         },
       ),
